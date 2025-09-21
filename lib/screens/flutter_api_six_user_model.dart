46aae2b6a3dafb24c7c7994f05826719edc92f0d
SuhAilManzoor <sohailmanzoorsario@gmail.com>
2025-09-21 19:46:12 +0500

    fetch and parse API response, Used for-in loop to convert each item to UserModel and storing them into allUsers list one by one, and Rendering UI with Listview.builder, ListTile and Card to display all Users

---
+++ lib/screens/flutter_api_six_user_model.dart
@@ -0,0 +1,81 @@
+import 'dart:convert';
+
+import 'package:fifteen_flutter_apis/models/best_user_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class FlutterApiOneThroghUserModel extends StatefulWidget {
+  const FlutterApiOneThroghUserModel({super.key});
+
+  @override
+  State<FlutterApiOneThroghUserModel> createState() =>
+      _FlutterApiOneThroghUserModelState();
+}
+
+class _FlutterApiOneThroghUserModelState
+    extends State<FlutterApiOneThroghUserModel> {
+  Future<List<UserModel>> getUsers() async {
+    List<UserModel> allUsers = [];
+    var url = Uri.https('jsonplaceholder.typicode.com', '/users');
+    var response = await http.get(url);
+    var responseBody = jsonDecode(response.body);
+
+    for (var i in responseBody) {
+      allUsers.add(UserModel.fromJson(i));
+    }
+    return allUsers;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: FutureBuilder<List<UserModel>>(
+          future: getUsers(),
+          builder: (BuildContext context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text("Error ${snapshot.error}"));
+            }
+
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (BuildContext context, index) {
+                  var allUsers = snapshot.data![index];
+                  return Card(
+                    shadowColor: Colors.grey,
+                    child: ListTile(
+                      leading: CircleAvatar(child: Text("${allUsers.id}")),
+                      title: Text(
+                        allUsers.name.isEmpty
+                            ? "No Name"
+                            : "Name: ${allUsers.name}",
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Username: ${allUsers.username}"),
+                          Text("Email: ${allUsers.email}"),
+                          Text("Phone: ${allUsers.phone}"),
+                          Text(
+                            "Address: ${allUsers.address.street} City ${allUsers.address.city}",
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+            return Center(child: Text("No Data Avilable"));
+          },
+        ),
+      ),
+    );
+  }
+}
